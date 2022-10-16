7dbed6db66f8c12878e2414f1dc4dd9e65142952
Adesh <adeshp948@gmail.com>
2022-10-16 02:09:24 -0400

    added operators in JS file

---
+++ w2/operators.js
@@ -0,0 +1,38 @@
+//operators in JS
+
+// Modular operator
+var x = 52, y=10;
+x = x%y;
+x
+
+
+//exponential operator
+var a = 10, b = 2;
+a = a**b;
+a
+
+//comparision operator
+var x = 3 == 3;
+x
+
+var x = 3 == "3"; //does not check data type
+x
+
+
+//strict operator "===" operator check data type before comparision
+var x = 3 === "3";
+x
+ 
+
+//unary operator
+var x="3";// string
+x= +x;
+x// number
+//here by using unary operator data type of x has been changed.
+
+
+
+ 
+
+//check all operators in mozilla docs on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+
