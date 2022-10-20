dd44f18203b4cd6c2d85dd28cb45deb78b6d515b
Adesh <adeshp948@gmail.com>
2022-10-20 03:56:31 -0400

    Strings in JS

---
+++ w2/StringInJS.js
@@ -0,0 +1,21 @@
+
+
+var favShow = "Game of Thrones";
+favShow
+
+console.log(favShow.length)
+console.log(favShow[0])
+
+console.log(favShow[favShow.length-1])
+
+console.log(favShow.indexOf("of"))
+
+console.log(favShow.slice(5,7))
+
+var srk="My namE Is KhaN!!!"
+
+console.log(srk)
+
+console.log(srk.toUpperCase())
+
+console.log(srk.toLowerCase())
